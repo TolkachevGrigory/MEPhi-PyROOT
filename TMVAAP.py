0c8316156f2f79182f9f441cda86f71b3c220b46
TolkachevGrigory <55763195+TolkachevGrigory@users.noreply.github.com>
2020-03-24 17:56:34 +0300

    Update TMVAAP.py

--- TMVAAP.py
+++ TMVAAP.py
@@ -14,32 +14,9 @@ import ROOT
 # In[2]:
 
 
-get_ipython().run_line_magic('jsroot', 'on')
-
-
-# In[3]:
-
-
-
-f1 = ROOT.TFile.Open('/eos/user/g/gtolkach/data/tau_mu_plusandminus_6variables.root')
-f2 = ROOT.TFile.Open('/eos/user/g/gtolkach/data/mu_plusandminus_6variables.root')
-f3 = ROOT.TFile.Open('/eos/user/g/gtolkach/data/data_mu_6variables.root')
-
-tree1 = f1.Get('NOMINAL')
-tree2 = f2.Get('NOMINAL')
-tree3 = f3.Get('NOMINAL')
-
-
-# In[4]:
-
-
 TMVA.Tools.Instance()
 TMVA.PyMethodBase.PyInitialize()
 reader = TMVA.Reader( "!Color:!Silent" )
-
-
-# In[5]:
-
 
 Pt = array('f',[0])
 MetE = array('f',[0])
@@ -49,118 +26,140 @@ Mt = array('f',[0])
 Eta = array('f',[0])
 
 reader.AddVariable( "Pt", Pt)
-tree1.SetBranchAddress("Pt", Pt)
-tree2.SetBranchAddress("Pt", Pt)
-tree3.SetBranchAddress("Pt", Pt)
-
 reader.AddVariable( "metE",MetE)
-tree1.SetBranchAddress("metE", MetE)
-tree2.SetBranchAddress("metE", MetE)
-tree3.SetBranchAddress("metE", MetE)
-
 reader.AddVariable( "dPhi",dPhi)
-tree1.SetBranchAddress("dPhi", dPhi)
-tree2.SetBranchAddress("dPhi", dPhi)
-tree3.SetBranchAddress("dPhi", dPhi)
-
 reader.AddVariable( "PhiMiss",PhiMiss)
-tree1.SetBranchAddress("PhiMiss", PhiMiss)
-tree2.SetBranchAddress("PhiMiss", PhiMiss)
-tree3.SetBranchAddress("PhiMiss", PhiMiss)
-
 reader.AddVariable( "Mt", Mt)
-tree1.SetBranchAddress("Mt", Mt)
-tree2.SetBranchAddress("Mt", Mt)
-tree3.SetBranchAddress("Mt", Mt)
-
 reader.AddVariable( "Eta",Eta)
-tree1.SetBranchAddress("Eta", Eta)
-tree2.SetBranchAddress("Eta", Eta)
-tree3.SetBranchAddress("Eta", Eta)
-
-
-# In[6]:
-
-
-
 reader.BookMVA("MLP",ROOT.TString("/eos/user/g/gtolkach/SWAN_projects/testpy/datasetV6newBDT/weights/TMVAClassification_MLP.weights.xml"))
 
 
-# In[7]:
+# In[3]:
 
 
-h1 = ROOT.TH1D("output_s","Blue=Signal, Red=Background;EvaluateMVA;Probability", 100,-0.5, 1.1)
-h2 = ROOT.TH1D("output_b","", 100, -0.4, 1.1)
-h3 = ROOT.TH1D("output_b","", 100, -0.4, 1.1)
+f1 = ROOT.TFile.Open('/eos/user/g/gtolkach/data/tau_mu_plusandminus_6variables_and_luminosity.root')
+f2 = ROOT.TFile.Open('/eos/user/g/gtolkach/data/data_mu_6variables_and_charge_minus.root')
+f3 = ROOT.TFile.Open('/eos/user/g/gtolkach/data/mu_plusandminus_6variables_and_luminosity.root')
+
+
+# In[40]:
+
+
+def rew(file, tp):
+    
+    tree = file.Get('NOMINAL')
+    tree.SetBranchAddress("Pt", Pt)
+    tree.SetBranchAddress("metE", MetE)
+    tree.SetBranchAddress("dPhi", dPhi)
+    tree.SetBranchAddress("PhiMiss", PhiMiss)
+    tree.SetBranchAddress("Mt", Mt)
+    tree.SetBranchAddress("Eta", Eta)
+    
+    h = ROOT.TH1D("h","", 20,-0.1, 1.1)
+    print(type(h))
+    print (tp.find('data'))
+    if tp.find('data') == -1:
+        h1 = file.Get('h_metadata')
+        bin_8 = h1.GetBinContent(8)
+        print(bin_8)
+        kfactor =  11.246151648051285
+        lumi2015 = 3219.56  # pb-1
+        for i in range(0,tree.GetEntries()):
+            tree.GetEntry(i)
+            Pt[0] = tree.Pt
+            MetE[0] = tree.metE
+            dPhi[0] = tree.dPhi
+            Mt[0] = tree.Mt
+            Eta[0] = tree.Eta
+            PhiMiss[0] = tree.PhiMiss
+            l = reader.EvaluateMVA("MLP")
+            h.Fill(l,tree.NOMINAL_pileup_combined_weight*tree.cross_section*tree.filter_efficiency*kfactor*lumi2015/bin_8)
+            #print(tree.NOMINAL_pileup_combined_weight*tree.cross_section*tree.filter_efficiency*tree.kfactor/bin_8)
+    else:
+        for i in range(0,tree.GetEntries()):
+            tree.GetEntry(i)
+            Pt[0] = tree.Pt
+            MetE[0] = tree.metE
+            dPhi[0] = tree.dPhi
+            Mt[0] = tree.Mt
+            Eta[0] = tree.Eta
+            PhiMiss[0] = tree.PhiMiss
+            l = reader.EvaluateMVA("MLP")
+            h.Fill(l)
+    return h
+
+
+# In[41]:
+
+
+h_mc_wtau = rew(f1, 'dd')
+
+
+# In[33]:
+
+
+h_data = rew(f2, 'data')
+
+
+# In[42]:
+
+
+h_mc_wmu = rew(f3, 'dd')
 
 
 # In[8]:
 
 
-for i in range(0,tree1.GetEntries()):
-    tree1.GetEntry(i)
-    Pt[0] = tree1.Pt
-    MetE[0] = tree1.metE
-    dPhi[0] = tree1.dPhi
-    Mt[0] = tree1.Mt
-    Eta[0] = tree1.Eta
-    PhiMiss[0] = tree1.PhiMiss
-    l = reader.EvaluateMVA("MLP")
-    h1.Fill(l)
+#c1 = ROOT.TCanvas() 
+#c1.SetLogy()
+#c1.cd()
+#h_mc_wmu.SetLineColor(ROOT.kRed)
+#h_mc_wtau.SetLineColor(ROOT.kYellow)
+#h_data.Draw('hist')
+#h_mc_wmu.Draw('hist ')
+#h_mc_wtau.Draw('hist same')
+#c1.Draw()
 
 
-# In[9]:
-
-
-for i in range(0, tree2.GetEntries()):
-    tree2.GetEntry(i)
-    Pt[0] = tree2.Pt
-    MetE[0] = tree2.metE
-    dPhi[0] = tree2.dPhi
-    Mt[0] = tree2.Mt
-    Eta[0] = tree2.Eta
-    PhiMiss[0] = tree2.PhiMiss
-    k = reader.EvaluateMVA("MLP")
-    h2.Fill(k)
-
-
-# In[10]:
-
-
-for i in range(0, tree3.GetEntries()):
-    tree3.GetEntry(i)
-    Pt[0] = tree3.Pt
-    MetE[0] = tree3.metE
-    dPhi[0] = tree3.dPhi
-    Mt[0] = tree3.Mt
-    Eta[0] = tree3.Eta
-    PhiMiss[0] = tree3.PhiMiss
-    k = reader.EvaluateMVA("MLP")
-    h3.Fill(k)
-
-
-# In[11]:
-
-
-
-#h1.Scale(1./h1.Integral())
-#h2.Scale(1./h2.Integral())
-#h3.Scale(1./h3.Integral())
-
-
-# In[12]:
+# In[43]:
 
 
 c1 = ROOT.TCanvas() 
+c1.Divide(1, 2)
 c1.SetLogy()
+ROOT.gStyle.SetOptStat(0)
 c1.cd()
+gPad = c1.cd(1)
+gPad.SetLogy()
 
-h1.SetLineColor(ROOT.kRed)
-h2.SetLineColor(ROOT.kYellow)
-h3.Draw("hist")
-h2.Draw("hist same")
-h1.Draw("hist same")
 
+h_mc_wtau.SetLineColor(ROOT.kRed)
+h_mc_wmu.SetLineColor(ROOT.kYellow)
+
+h_mc_wmu.SetTitle("mc_wmu")
+h_data.SetTitle('data')
+h_mc_wtau.SetTitle("mc_wtau")
+
+h_data.Draw("hist ")
+h_mc_wtau.Draw("hist same")
+h_mc_wmu.Draw("hist same")
+
+
+
+ROOT.gPad.BuildLegend(1.0,0.8,0.8,0.5,"","")
+h_add = ROOT.TH1D("add","", 20, -0.1, 1.1)
+h_divide = ROOT.TH1D("divide","", 20, -0.1, 1.1)
+h_add.Add(h_mc_wtau, h_mc_wmu, 1, 1)
+h_divide.Divide(h_data, h_add, 1, 1)
+c1.cd(2)
+h_divide.Draw("E1")
+c1.Update()
+c1.Draw()
+h_add.Add(h_mc_wtau, h_mc_wmu, 1, 1)
+h_divide.Divide(h_data, h_add, 1, 1)
+c1.cd(2)
+h_divide.Draw("E1")
+c1.Update()
 c1.Draw()
 
 
